adb2ae99540f700eb9876cb79949aaef5bfa45dd
antdp425 <antdp425@aol.com>
2020-05-04 15:39:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,57 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    recipe = Recipe.new(params)
+    if recipe.save
+      redirect "/recipes/#{recipe.id}"
+    else
+      redirect '/recipes/new'
+    end
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      erb :show
+    else
+      redirect '/recipes'
+    end
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe
+      erb :edit
+    else
+      redirect '/recipes'
+    end
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe.update(params.except(:id, :_method))
+      redirect "/recipes/#{recipe.id}"
+    else
+      redirect "/recipes/#{recipe.id}/edit"
+    end
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by(id: params[:id])
+    if @recipe.delete
+      redirect "/recipes"
+    else
+      redirect "/recipes"   
+    end   
+  end
 end
