606be0153af476d7a394d7ba97207a72a918e780
proiacm <proiacm@gmail.com>
2020-05-07 22:57:55 -0600

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,1 +1,15 @@
-#Placeholder for a model
+class Recipe < ActiveRecord::Base
+    # attr_accessor :name, :ingredients, :cook_time 
+    # RECIPES = []
+
+    # def initialize(params)
+    #     @name = params[:name]
+    #     @ingredients = params[:ingredients] 
+    #     @cook_time = params[:cook_time]
+    #     RECIPES << self
+    # end
+
+    # def self.all 
+    #     RECIPES 
+    # end
+end
